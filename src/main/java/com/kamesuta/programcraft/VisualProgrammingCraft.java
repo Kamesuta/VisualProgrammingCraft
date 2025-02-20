65e00e2e9d6830e9ec2bd7f60b8febced06ec445
Kamesuta <kamesuta@gmail.com>
2025-02-20 23:56:28 +0900

    Luaj Sandbox

---
+++ src/main/java/com/kamesuta/programcraft/VisualProgrammingCraft.java
@@ -0,0 +1,17 @@
+package com.kamesuta.programcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VisualProgrammingCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
