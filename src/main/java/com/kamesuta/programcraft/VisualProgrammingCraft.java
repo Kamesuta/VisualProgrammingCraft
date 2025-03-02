e0f4a98e02d562b3d253871e06f9648942c0777e
Kamesuta <kamesuta@gmail.com>
2025-03-02 17:48:14 +0900

    Lua実行

--- src/main/java/com/kamesuta/programcraft/VisualProgrammingCraft.java
+++ src/main/java/com/kamesuta/programcraft/VisualProgrammingCraft.java
@@ -1,17 +1,155 @@
 package com.kamesuta.programcraft;
 
+import com.kamesuta.programcraft.lua.LuaMachine;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.OneArgFunction;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public final class VisualProgrammingCraft extends JavaPlugin {
+    public static VisualProgrammingCraft instance;
+    public static Logger logger;
+    public static String biosText;
+    private static Map<String, LuaMachine> luaMachines = new HashMap<>();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
+        logger = getLogger();
 
+        // Load the bios
+        try {
+            biosText = loadTextResource("bios.lua");
+        } catch (IOException e) {
+            logger.severe("Could not load bios.lua: " + e.getMessage());
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!command.getName().equalsIgnoreCase("pico")) {
+            return false;
+        }
+
+        if (args.length < 1) {
+            sender.sendMessage("Usage: /pico <create|destroy|list|run>");
+            return true;
+        }
+        String subCommand = args[0];
+
+        if (subCommand.equalsIgnoreCase("list")) {
+            sender.sendMessage("Machines:");
+            for (String machineName : luaMachines.keySet()) {
+                sender.sendMessage("  " + machineName);
+            }
+            return true;
+        }
+
+        if (args.length < 2) {
+            sender.sendMessage("Usage: /pico <name> <create|destroy|list|run>");
+            return true;
+        }
+        String name = args[1];
+
+        if (subCommand.equalsIgnoreCase("create")) {
+            if (luaMachines.containsKey(name)) {
+                sender.sendMessage("Machine already exists: " + name);
+                return true;
+            }
+            LuaMachine machine = new LuaMachine();
+            machine.addAPI("println", new OneArgFunction() {
+                @Override
+                public LuaValue call(LuaValue luaValue) {
+                    sender.sendMessage(luaValue.tojstring());
+                    return LuaValue.NIL;
+                }
+            });
+            luaMachines.put(name, machine);
+            sender.sendMessage("Machine created: " + name);
+            return true;
+        } else if (subCommand.equalsIgnoreCase("destroy")) {
+            LuaMachine machine = luaMachines.get(name);
+            if (machine == null) {
+                sender.sendMessage("Machine does not exist: " + name);
+                return true;
+            }
+            machine.unload();
+            luaMachines.remove(name);
+            sender.sendMessage("Machine destroyed: " + name);
+            return true;
+        } else if (subCommand.equalsIgnoreCase("run")) {
+            if (args.length < 3) {
+                sender.sendMessage("Usage: /pico run <name> <code>");
+                return true;
+            }
+            LuaMachine machine = luaMachines.get(name);
+            if (machine == null) {
+                sender.sendMessage("Machine does not exist: " + name);
+                return true;
+            }
+            if (!machine.isFinished()) {
+                sender.sendMessage("Machine is already running: " + name);
+                return true;
+            }
+
+            String code = args[2];
+            machine.loadBios(biosText + "\n" + code);
+            machine.handleEvent(null, null);
+            return true;
+        } else {
+            sender.sendMessage("Usage: /pico <create|destroy|list|run> <name>");
+            return true;
+        }
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        if (!command.getName().equalsIgnoreCase("pico")) {
+            return null;
+        }
+
+        if (args.length == 1) {
+            return List.of("create", "destroy", "list", "run");
+        } else if (args.length == 2) {
+            return luaMachines.keySet().stream()
+                    .filter(name -> name.startsWith(args[1]))
+                    .collect(Collectors.toList());
+        }
+
+        return null;
+    }
+
+    private String loadTextResource(String filename) throws IOException {
+        // Load the bios
+        try (Reader reader = getTextResource(filename)) {
+            if (reader == null) {
+                throw new IOException("Could not find file: " + filename);
+            }
+            return new BufferedReader(reader)
+                    .lines()
+                    .collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new IOException("Could not read file: " + filename);
+        }
     }
 }
---
+++ src/main/java/com/kamesuta/programcraft/lua/LuaMachine.java
@@ -0,0 +1,197 @@
+package com.kamesuta.programcraft.lua;
+
+import com.kamesuta.programcraft.VisualProgrammingCraft;
+import org.bukkit.Bukkit;
+import org.luaj.vm2.*;
+import org.luaj.vm2.lib.OneArgFunction;
+import org.luaj.vm2.lib.ZeroArgFunction;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.stream.Stream;
+
+public class LuaMachine {
+    private LuaValue m_globals;
+    private LuaValue m_loadString;
+    private LuaValue m_assert;
+    private LuaValue m_coroutine_create;
+    private LuaValue m_coroutine_resume;
+    private LuaValue m_coroutine_yield;
+
+    private LuaValue m_mainRoutine;
+    private String m_eventFilter;
+    private String m_softAbortMessage;
+    private String m_hardAbortMessage;
+
+    public LuaMachine() {
+        // Create an environment to run in
+        m_globals = JsePlatform.debugGlobals();
+        m_loadString = m_globals.get("load");
+        m_assert = m_globals.get("assert");
+
+        LuaValue coroutine = m_globals.get("coroutine");
+        final LuaValue native_coroutine_create = coroutine.get("create");
+
+        LuaValue debug = m_globals.get("debug");
+        final LuaValue debug_sethook = debug.get("sethook");
+
+        coroutine.set("create", new OneArgFunction() {
+            @Override
+            public LuaValue call(LuaValue value) {
+                final LuaThread thread = native_coroutine_create.call(value).checkthread();
+                debug_sethook.invoke(new LuaValue[]{
+                        thread,
+                        new ZeroArgFunction() {
+                            @Override
+                            public LuaValue call() {
+                                String hardAbortMessage = m_hardAbortMessage;
+                                if (hardAbortMessage != null) {
+                                    // スレッド停止
+                                    if (thread.state.function == null) {
+                                        throw new LuaError("cannot yield main thread");
+                                    }
+                                    thread.state.lua_yield(LuaValue.NIL);
+                                }
+                                return LuaValue.NIL;
+                            }
+                        },
+                        LuaValue.NIL,
+                        LuaValue.valueOf(100000)
+                });
+                return thread;
+            }
+        });
+
+        m_coroutine_create = coroutine.get("create");
+        m_coroutine_resume = coroutine.get("resume");
+        m_coroutine_yield = coroutine.get("yield");
+
+        // Remove globals we don't want to expose
+        m_globals.set("collectgarbage", LuaValue.NIL);
+        m_globals.set("dofile", LuaValue.NIL);
+        m_globals.set("loadfile", LuaValue.NIL);
+        m_globals.set("module", LuaValue.NIL);
+        m_globals.set("require", LuaValue.NIL);
+        m_globals.set("package", LuaValue.NIL);
+        m_globals.set("io", LuaValue.NIL);
+        m_globals.set("os", LuaValue.NIL);
+        m_globals.set("print", LuaValue.NIL);
+        m_globals.set("luajava", LuaValue.NIL);
+        m_globals.set("debug", LuaValue.NIL);
+        m_globals.set("newproxy", LuaValue.NIL);
+        m_globals.set("__inext", LuaValue.NIL);
+
+        // Add version globals
+        m_globals.set("_VERSION", "Lua 5.1");
+        String version = VisualProgrammingCraft.instance.getDescription().getVersion();
+        m_globals.set("_HOST", "VisualProgrammingCraft " + version + " (Minecraft " + Bukkit.getServer().getVersion() + ")");
+
+        // Our main function will go here
+        m_mainRoutine = null;
+        m_eventFilter = null;
+
+        m_softAbortMessage = null;
+        m_hardAbortMessage = null;
+    }
+
+    public void addAPI(String name, LuaValue api) {
+        m_globals.set(name, api);
+    }
+
+    public void loadBios(String biosText) {
+        // Begin executing a file (ie, the bios)
+        if (m_mainRoutine != null) {
+            return;
+        }
+
+        try {
+            // Load it
+            LuaValue program = m_assert.call(m_loadString.call(
+                    LuaValue.valueOf(biosText), LuaValue.valueOf("bios.lua")
+            ));
+            m_mainRoutine = m_coroutine_create.call(program);
+        } catch (LuaError e) {
+            VisualProgrammingCraft.logger.log(Level.WARNING, "Could not load bios.lua ", e);
+            if (m_mainRoutine != null) {
+                ((LuaThread) m_mainRoutine).abandon();
+                m_mainRoutine = null;
+            }
+        }
+    }
+
+    public void handleEvent(String eventName, LuaValue[] arguments) {
+        if (m_mainRoutine == null) {
+            return;
+        }
+
+        if (m_eventFilter != null && eventName != null && !eventName.equals(m_eventFilter) && !eventName.equals("terminate")) {
+            return;
+        }
+
+        try {
+            LuaValue[] resumeArgs = Stream.of(
+                            Stream.of(m_mainRoutine),
+                            eventName == null ? Stream.<LuaValue>empty() : Stream.of(LuaValue.valueOf(eventName)),
+                            Arrays.stream(arguments == null ? new LuaValue[0] : arguments)
+                    )
+                    .flatMap(s -> s)
+                    .toArray(LuaValue[]::new);
+
+            Varargs results = m_coroutine_resume.invoke(LuaValue.varargsOf(resumeArgs));
+            if (m_hardAbortMessage != null) {
+                throw new LuaError(m_hardAbortMessage);
+            } else if (!results.arg1().checkboolean()) {
+                throw new LuaError(results.arg(2).checkstring().toString());
+            } else {
+                LuaValue filter = results.arg(2);
+                if (filter.isstring()) {
+                    m_eventFilter = filter.toString();
+                } else {
+                    m_eventFilter = null;
+                }
+            }
+
+            LuaThread mainThread = (LuaThread) m_mainRoutine;
+            if (mainThread.getStatus().equals("dead")) {
+                m_mainRoutine = null;
+            }
+        } catch (LuaError e) {
+            ((LuaThread) m_mainRoutine).abandon();
+            m_mainRoutine = null;
+        } finally {
+            m_softAbortMessage = null;
+            m_hardAbortMessage = null;
+        }
+    }
+
+    public void softAbort(String abortMessage) {
+        m_softAbortMessage = abortMessage;
+    }
+
+    public void hardAbort(String abortMessage) {
+        m_softAbortMessage = abortMessage;
+        m_hardAbortMessage = abortMessage;
+    }
+
+    public boolean isFinished() {
+        return (m_mainRoutine == null);
+    }
+
+    public void unload() {
+        if (m_mainRoutine != null) {
+            LuaThread mainThread = (LuaThread) m_mainRoutine;
+            mainThread.abandon();
+            m_mainRoutine = null;
+        }
+    }
+
+    private void tryAbort() throws LuaError {
+        String abortMessage = m_softAbortMessage;
+        if (abortMessage != null) {
+            m_softAbortMessage = null;
+            m_hardAbortMessage = null;
+            throw new LuaError(abortMessage);
+        }
+    }
+}
